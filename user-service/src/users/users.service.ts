c4ffdf625b87115d6951ba23881c6394bd0744f6
Vladimir <pestov.web@yandex.ru>
2024-11-19 23:23:07 +0300

    feat: user issues service

---
+++ user-service/src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async resetIssuesFlag(): Promise<number> {
+    const result = await this.userRepository
+      .createQueryBuilder()
+      .update(User)
+      .set({ hasIssues: false })
+      .where('hasIssues = :hasIssues', { hasIssues: true })
+      .returning('id')
+      .execute();
+
+    // Возвращаем количество обновленных записей
+    return result.affected || 0;
+  }
+}
