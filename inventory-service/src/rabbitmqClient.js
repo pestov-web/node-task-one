3d602f663fcd3f7ff3c3ce9ff4c501f539cb344c
Vladimir <pestov.web@yandex.ru>
2024-11-20 21:41:36 +0300

    fix: rabbitMQ client

--- inventory-service/src/rabbitmqClient.js
+++ inventory-service/src/rabbitmqClient.js
@@ -3,10 +3,12 @@ const amqp = require("amqplib");
 const RABBIT_URL = process.env.RABBITMQ_URL;
 const QUEUE_NAME = process.env.QUEUE_NAME;
 
+let channel; // Объявляем глобальную переменную для канала
+
 const connectRabbitMQ = async () => {
   try {
     const connection = await amqp.connect(RABBIT_URL);
-    const channel = await connection.createChannel();
+    channel = await connection.createChannel(); // Обновляем глобальную переменную
     await channel.assertQueue(QUEUE_NAME, { durable: true });
     console.log(`Connected to RabbitMQ ${QUEUE_NAME}`);
   } catch (error) {
@@ -16,6 +18,11 @@ const connectRabbitMQ = async () => {
 
 const sendMessage = async (message) => {
   try {
+    if (!channel) {
+      throw new Error(
+        "RabbitMQ channel is not initialized. Call connectRabbitMQ first."
+      );
+    }
     channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)), {
       persistent: true,
     });
